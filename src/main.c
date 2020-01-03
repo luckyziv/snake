9295630974a870fdba361e3bddb1f2790134be00
liujiaqi <1063871905@qq.com>
2020-01-03 17:09:34 +0800

    add draw

--- src/main.c
+++ src/main.c
@@ -6,12 +6,20 @@
 #include <fcntl.h>	/* open() */
 #include <linux/fb.h>	/* struct fb_var_screeninfo */
 #include <sys/ioctl.h>	/* ioctl() */
+#include <sys/mman.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+#include <termios.h>
+#include <fcntl.h>
 
 #include "typedef.h"
 
 struct fb_var_screeninfo vinfo;
 
+#define RGB888(r, g, b) ((0xff & r) << 16 | (0xff & g) << 8 | (b & 0xff))
 void show(void);
+void draw_side(unsigned long* addr);
 
 int main(int argc, char *argv)
 {
@@ -33,9 +41,22 @@ int main(int argc, char *argv)
 	}
 	
 	/* step3 */
+#if 0
 	printf("vinfo.xres: %d\r\n", vinfo.xres);
 	printf("vinfo.yres: %d\r\n", vinfo.yres);
 	printf("vinfo.bits_per_pixel: %d\r\n", vinfo.bits_per_pixel);
+#endif
+	
+	unsigned long* addr = mmap(NULL, 
+					(vinfo.xres*vinfo.yres*vinfo.bits_per_pixel>>3),
+					PROT_READ | PROT_WRITE,
+					MAP_SHARED,
+					fd,
+					0);
+
+	draw_side(addr);
+
+	while(1);
 
 	return 0;
 }
@@ -45,3 +66,31 @@ void show(void)
 {
 	printf("[%s] running...\r\n", __func__);
 }
+
+
+void draw_side(unsigned long* addr)
+{
+	int i, j;
+	
+	for(i = 180; i < 200; i++){
+		for(j = 190; j<410; j++)
+			*(addr + i*688 + j) = RGB888(0, 0, 0xff);
+	}
+
+	for(i = 600; i < 620; i++){
+		for(j = 190; j<410; j++)
+			*(addr + i*688 + j) = RGB888(0, 0, 0xff);
+	}
+
+	for(i = 200; i < 600; i++){
+		for(j = 190; j<200; j++)
+			*(addr + i*688 + j) = RGB888(0, 0, 0xff);
+	}
+
+	for(i = 200; i < 600; i++){
+		for(j = 400; j<410; j++)
+			*(addr + i*688 + j) = RGB888(0, 0, 0xff);
+	}
+}
+
+
